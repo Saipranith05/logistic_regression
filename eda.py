2bb34f56fc269e8281e3737ac38d7eeee05b5f65
git_demo <spranith49@gmail.com>
2024-10-15 14:22:38 +0530

    v4

--- eda.py
+++ eda.py
@@ -3,39 +3,45 @@ import mysql.connector
 import numpy as np
 import joblib
 
-model = joblib.load("Logistic.pkl")
-
-# mysql connection
-
+model = joblib.load("logistic.pkl")
+# MySQL connection
 def connect_to_db():
     return mysql.connector.connect(
         host="localhost",
         user="root",  # Change this
         password="Saipranith@2003",  # Change this
-        database="heart_data"   
+        database="heart_disease_data"
     )
-    
-    
+
 # Streamlit app
 st.title("Heart Disease Prediction")
 
+# User input fields
 age = st.number_input('Age', min_value=1, max_value=120)
-sex = st.selectbox('sex (0: Male, 1: Female)', [0, 1])
-bp = st.number_input('Blood Pressure (bp)', min_value = 80, max_value = 200)
+sex = st.selectbox('Sex (0: Female, 1: Male)', [0, 1])
+bp = st.number_input('Blood Pressure (bp)', min_value=80, max_value=200)
 cholesterol = st.number_input('Cholesterol (mg/dL)', min_value=100, max_value=600)
 
 if st.button('Predict'):
+    # Prepare data for prediction
     user_data = np.array([[age, sex, bp, cholesterol]])
-    prediction = model.predict(user_data)[0]
+    
+    # Make prediction
+    prediction = model.predict(user_data)[0] # [1 or 0]  [0]
     
     st.write(f"Prediction: {'Heart Disease' if prediction == 1 else 'No Heart Disease'}")
     
-    #store user in database
-    
-    db = connect_to_db()
+    db = connect_to_db() # mysql.conector.connect.cursor()
     cursor = db.cursor()
     
-    query = """"""
+    query = """
     INSERT INTO user_inputs (age, sex, bp, cholesterol, heart_disease)
     VALUES (%s, %s, %s, %s, %s)
+    """
+    cursor.execute(query, (age, sex, bp, cholesterol, int(prediction)))
+    db.commit()
     
+    st.write("Data saved to the database.")
+    
+    cursor.close()
+    db.close()
